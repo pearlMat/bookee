7566ed721080db169bfa86b863550e3e9a8f12c8
pearlMat <pearllyt@gmail.com>
2024-01-06 20:50:52 +0100

    created app base files

---
+++ cmd/api/books.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	
+	"net/http"
+	
+)
+
+func (app *application) createBookHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "create a new book")
+}
+
+func (app *application) showBookHandler(w http.ResponseWriter, r *http.Request) {
+
+	id, err := app.readIDParam(r)
+	if err != nil {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "show the details of a book %d\n", id)
+}
